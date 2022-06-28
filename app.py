212a90977484786916043af3156559fb7a1970a0
artem iliushin <artemiliushin@gmail.com>
2022-06-27 23:33:10 -0500

    saving final files

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask import Flask, jsonify, render_template
 #################################################
 # Database Setup
 #################################################
-connection_string = "postgres:post2022@localhost:5432/energy_db"
+connection_string = "postgres:postgres@localhost:5432/energy_db"
 engine = create_engine(f'postgresql://{connection_string}')
 
 # reflect an existing database into a new model
@@ -26,7 +26,7 @@ energy = Base.classes.energy
 #################################################
 # Flask Setup
 #################################################
-app = Flask(__name__)
+app = Flask(__name__,static_url_path="/static")
 
 
 #################################################
@@ -36,7 +36,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     """List all available api routes."""
-    return render_template('index_rtp.html')
+    return render_template('index.html')
 
 @app.route("/map")
 def us_map():
@@ -88,116 +88,3 @@ def renewable_energy():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import numpy as np
-
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
-
-# from flask import Flask, jsonify, render_template
-
-
-# #################################################
-# # Database Setup
-# #################################################
-# #####engine = create_engine("sqlite:///titanic.sqlite")
-# engine = create_engine('postgresql://postgres:Murphy006@localhost:5432/energy')
-
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(engine, reflect=True)
-
-# # Save reference to the table
-# Penergy = Base.classes.potential_energy
-
-# #################################################
-# # Flask Setup
-# #################################################
-# app = Flask(__name__)
-
-
-# #################################################
-# # Flask Routes
-# #################################################
-
-# @app.route("/")
-# def welcome():
-#     """List all available api routes."""
-#     return render_template('index.html')
-
-
-# @app.route("/api/v1.0/energy")
-# def names():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     """Return a list of all passenger names"""
-#     # Query all passengers
-#     results = session.query(Penergy).all()
-
-#     session.close()
-
-#     all_passengers = []
-#     for name in results:
-#         passenger_dict = {}
-#         passenger_dict["states"] = name
-#         all_passengers.append(passenger_dict)
-
-#     # Convert list of tuples into normal list
-#     ####all_names = list(np.ravel(results))
-
-#     return all_passengers
-
-
-# # @app.route("/api/v1.0/passengers")
-# # def passengers():
-# #     # Create our session (link) from Python to the DB
-# #     session = Session(engine)
-
-# #     """Return a list of passenger data including the name, age, and sex of each passenger"""
-# #     # Query all passengers
-# #     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-# #     session.close()
-
-# #     # Create a dictionary from the row data and append to a list of all_passengers
-# #     all_passengers = []
-# #     for name, age, sex in results:
-# #         passenger_dict = {}
-# #         passenger_dict["name"] = name
-# #         passenger_dict["age"] = age
-# #         passenger_dict["sex"] = sex
-# #         all_passengers.append(passenger_dict)
-
-# #     return jsonify(all_passengers)
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
